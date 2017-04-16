1b4c724bb16945d89738d1c6b0cb39479ace03d8
Emily Fox <emily.r.fox@hotmail.com>
2017-04-16 12:58:51 -0400

    Add files via upload

--- db/migrations/001-initial-schema.sql
+++ db/migrations/001-initial-schema.sql
@@ -5,17 +5,17 @@ CREATE TABLE user(
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	email TEXT NOT NULL,
-	password TEXT NOT NULL,
-	profile_pic TEXT
+	password TEXT NOT NULL
+	-- profile_pic TEXT
 );
 
 CREATE TABLE followers(
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	follow_id INTEGER PRIMARY KEY AUTOINCREMENT
+	id INTEGER,
+	follow_id INTEGER 
 );
 
 CREATE TABLE activity(
-	user_id INTEGER NOT NULL,
+	id INTEGER NOT NULL,
 	activity_id INTEGER NOT NULL,
 	activity_payload TEXT
 );
@@ -29,15 +29,14 @@ INSERT into user (first_name, last_name, email, password) VALUES ('mario', 'rang
 INSERT into user (first_name, last_name, email, password) VALUES ('taq', 'karim', 'taq@nycda.com', 'taqrules');
 INSERT into user (first_name, last_name, email, password) VALUES ('baba', 'k', 'baba@nycda.com', 'ilovedrake');
 
-INSERT into followers (user_id, follow_id) VALUES (1, 1);
-INSERT into followers (user_id, follow_id) VALUES (2, 1);
-INSERT into followers (user_id, follow_id) VALUES (3, 1);
-INSERT into followers (user_id, follow_id) VALUES (3, 2); 
-INSERT into followers (user_id, follow_id) VALUES (4, 2);
+INSERT into followers (id, follow_id) VALUES (1, 1);
+INSERT into followers (id, follow_id) VALUES (2, 1);
+INSERT into followers (id, follow_id) VALUES (3, 1);
+INSERT into followers (id, follow_id) VALUES (4, 2); 
+INSERT into followers (id, follow_id) VALUES (5, 2);
 
-INSERT into activity (activity_payload) VALUES ('like');
-INSERT into activity (activity_payload) VALUES ('checkin');
-INSERT into activity (activity_payload) VALUES ('comment');
+INSERT into activity (id, activity_id) VALUES (1, 3);
+INSERT into activity (id, activity_id) VALUES (2, 4);
 
 -- DOWN
 
