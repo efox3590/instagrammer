aeebf72b8c6638bb492dc832eebe71a43f164adc
Emily Fox <emily.r.fox@hotmail.com>
2017-04-15 20:38:43 -0400

    Add files via upload

--- db/migrations/001-initial-schema.sql
+++ db/migrations/001-initial-schema.sql
@@ -1,7 +1,7 @@
 -- UP
 
-CREATE TABLE Users(
-	id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE user(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	email TEXT NOT NULL,
@@ -9,20 +9,38 @@ CREATE TABLE Users(
 	profile_pic TEXT
 );
 
-CREATE TABLE Followers(
-	user_id INTEGER,
-	follow_id INTEGER
+CREATE TABLE followers(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	follow_id INTEGER PRIMARY KEY AUTOINCREMENT
 );
 
--- this might need some tweaking
-CREATE TABLE Activity(
+CREATE TABLE activity(
 	user_id INTEGER NOT NULL,
 	activity_id INTEGER NOT NULL,
-	activity_payload TEXT NOT NULL
+	activity_payload TEXT
 );
+
+-- activity_type_id - something for future proofing your app; if you wanted to display more than just posts (ie: likes, checkins).
+
+
+INSERT into user (first_name, last_name, email, password) VALUES ('emily', 'fox', 'emily@nycda.com', 'testing1234');
+INSERT into user (first_name, last_name, email, password) VALUES ('linda', 'yu', 'linda@nycda.com', 'chickenricer');
+INSERT into user (first_name, last_name, email, password) VALUES ('mario', 'rangel', 'mario@nycda.com', 'mario5678');
+INSERT into user (first_name, last_name, email, password) VALUES ('taq', 'karim', 'taq@nycda.com', 'taqrules');
+INSERT into user (first_name, last_name, email, password) VALUES ('baba', 'k', 'baba@nycda.com', 'ilovedrake');
+
+INSERT into followers (user_id, follow_id) VALUES (1, 1);
+INSERT into followers (user_id, follow_id) VALUES (2, 1);
+INSERT into followers (user_id, follow_id) VALUES (3, 1);
+INSERT into followers (user_id, follow_id) VALUES (3, 2); 
+INSERT into followers (user_id, follow_id) VALUES (4, 2);
+
+INSERT into activity (activity_payload) VALUES ('like');
+INSERT into activity (activity_payload) VALUES ('checkin');
+INSERT into activity (activity_payload) VALUES ('comment');
 
 -- DOWN
 
-DROP TABLE Users;
-DROP TABLE Followers;	
-DROP TABLE Activity;
+DROP TABLE user;
+DROP TABLE followers;	
+DROP TABLE activity;
