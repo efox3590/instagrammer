eb91e359ecfbd3ca234be280a39870a69215b6eb
linda <linda.listmail@gmail.com>
2017-04-18 10:10:45 -0400

    added preliminary render function to main.js

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -1,27 +1,48 @@
 -- UP
 
-CREATE TABLE Users(
-	id INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE user(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
 	email TEXT NOT NULL,
 	password TEXT NOT NULL,
+	profile_pic TEXT
 );
 
-CREATE TABLE Follows(
-	user_id INTEGER,
-	follow_id INTEGER
+CREATE TABLE followers(
+	id INTEGER NOT NULL,
+	follow_id INTEGER NOT NULL
 );
 
--- this might need some tweaking
-CREATE TABLE Activity(
-	user_id INTEGER NOT NULL,
+CREATE TABLE activity(
+	id INTEGER NOT NULL,
 	activity_id INTEGER NOT NULL,
-	activity_payload TEXT NOT NULL
+	activity_payload TEXT
 );
+
+-- activity_type_id - something for future proofing your app; if you wanted to display more than just posts (ie: likes, checkins).
+
+
+INSERT into user (first_name, last_name, email, password) VALUES ('emily', 'fox', 'emily@nycda.com', 'testing1234');
+INSERT into user (first_name, last_name, email, password) VALUES ('linda', 'yu', 'linda@nycda.com', 'chickenricer');
+INSERT into user (first_name, last_name, email, password) VALUES ('mario', 'rangel', 'mario@nycda.com', 'mario5678');
+INSERT into user (first_name, last_name, email, password) VALUES ('taq', 'karim', 'taq@nycda.com', 'taqrules');
+INSERT into user (first_name, last_name, email, password) VALUES ('baba', 'k', 'baba@nycda.com', 'ilovedrake');
+
+INSERT into followers (id, follow_id) VALUES (1, 1);
+INSERT into followers (id, follow_id) VALUES (2, 1);
+INSERT into followers (id, follow_id) VALUES (3, 1);
+INSERT into followers (id, follow_id) VALUES (4, 2); 
+INSERT into followers (id, follow_id) VALUES (5, 2);
+
+INSERT into activity (id, activity_id) VALUES (1, 3);
+INSERT into activity (id, activity_id) VALUES (2, 4);
+INSERT into activity (id, activity_id) VALUES (3, 6);
+INSERT into activity (id, activity_id) VALUES (4, 7);
+INSERT into activity (id, activity_id) VALUES (5, 8);
 
 -- DOWN
 
-DROP TABLE Users;
-DROP TABLE Follows;	
-DROP TABLE Activity;
+DROP TABLE user;
+DROP TABLE followers;	
+DROP TABLE activity;
--- public/js/main.js
+++ public/js/main.js
@@ -69,6 +69,151 @@
 		});
 	} // DELETE
 
+	// if (document.querySelector('.js-reg-fname') !== null){
+	if (location.pathname === '/register.html') {
+
+		const fname = document.querySelector('.js-reg-fname');
+		const lname = document.querySelector('.js-reg-lname');
+		const email = document.querySelector('.js-reg-email');
+		const pw1 = document.querySelector('.js-reg-pw1');
+		const pw2 = document.querySelector('.js-reg-pw2');
+		const btn = document.querySelector('.js-reg-btn');
+		const message = document.querySelector('.js-reg-message');
+
+		btn.addEventListener('click', (e) => {
+			e.preventDefault();
+			if (pw1.value !== pw2.value) {
+				console.log('pw1 is :', pw1.value);
+				console.log('pw2 is :', pw2.value)
+				message.innerHTML = 'Passwords do not match.'
+				pw1.focus();
+			} 
+			else {	//can be deleted. just for testing
+				message.innerHTML = 'good job. passwords match'
+			}		//delete this silliness
+				
+			// route to add user to db
+			POST('/api/user', )
+
+		}) // event listener
+
+	}	// register.html
+
+	// if (document.querySelector('.js-log-email') !== null){
+	if (location.pathname === '/login.html') {
+
+		const email = document.querySelector('.js-log-email');
+		const pw = document.querySelector('.js-log-pw');
+		const btn = document.querySelector('.js-log-btn')
+
+		btn.addEventListener('click', (e) => {
+
+		run passport authentication logic !!! 
+
+		POST('/api/')
+
+		})
+			
+	}	// login.html
+
+	if (location.pathname === '/feed.html') {
+
+
+
+		function render(postItems) {
+			const container = document.querySelector('.js-postlist');
+			container.innerHTML = '';
+			postItems = postItems.reverse();
+// how do we get back this array from sqlite?
+
+			// console.log('postItems :',postItems);
+			for (const postItem of postItems) {
+// need field in sql db to store time so entries can be sorted by time 
+// and time can be displayed on posts via moment.js
+
+
+				// const fullContent = `${postItem.data.post}`;
+				// let hour = `${postItem.data.hour}`
+			 //    const ampm = hour >= 12 ? 'PM' : 'AM';
+			 //    hour = hour % 12;
+			 //    let minute = `${postItem.data.minute}`;
+			 //    minute = parseInt(minute, 10) > 10 ? minute : "0".concat(minute);
+			 //    let second = `${postItem.data.second}`;
+			 //    second = parseInt(second, 10) > 10 ? second : "0".concat(second);
+
+			 //    const timeStamp = `${postItem.data.month + 1}/${postItem.data.day}/${postItem.data.year}
+		  //     ${hour}:${minute}:${second} ${ampm} `;
+		    const li = document.createElement('li');
+			li.classList.add('list-group-item', 'postlist-item', `js-blog-item-${postItem.id}`);
+	// need ${vars} for: image url, caption, commenter_id, commenter_comment
+			const img_url = postItem.data.img_url;
+			const caption = postItem.data.caption;
+			const timeStamp = moment(postItem.data.when).format('dddd, MMMM DD, YYYY h:mm a');
+			// const comm_id = 
+			// const comm_comment = 
+		    li.innerHTML = `
+<figure>
+	<div class="item-image">
+	// <img src="../assets/puppy.jpg" alt="Puppy" class="img-responsive">
+	<img src=`${img_url}` class="img-responsive">
+
+	<figcaption>
+	// Bear was walking in the water! So cute!
+	$[caption}
+	</figcaption>
+	</div><!-- item image -->
+	<div class="likes">
+	<span class="glyphicon glyphicon-heart-empty"></span> Like
+	</div>
+</figure> 
+
+<section>
+	<article>
+		<ul class="comments-list">
+			<li class="comment-item">
+				// <p><strong>Bob Dylan:</strong> What a cute puppy! </p>
+				<p><strong>${comm_id}:</strong> ${comm_comment} </p>
+			</li>
+		</ul>
+		  <div class="col-lg-6">
+		    <div class="input-group">
+		      <input type="text" class="form-control input-text js-comment-input" placeholder="Add a Comment">
+		      <button class="js-comment-btn btn btn-primary">Add</button>
+		    </div><!-- /input-group -->
+		  </div><!-- /.col-lg-6 -->
+	</article>
+</section>
+		    `; // end li.innerHTML
+		    
+
+		    //need to isolate proper element
+		 //    if (postItem.data.isLiked) {
+			// 	li.innerHTML += `<span class="glyphicon glyphicon-heart js-like"></span>`
+			// }
+			// else {
+			// 	li.innerHTML += `<span class="glyphicon glyphicon-heart-empty js-like"></span>`
+				// }
+
+			} // for /of loop
+
+		} // render()
+
+
+	} // feed.html
+
+
+
+	// using moment
+	// function getDate() {
+	// 	let date = new Date();
+	// 	console.log(date);
+	// 	date = moment(date).format('dddd, MMMM DD, YYYY h:mm a');
+	// 	console.log(date);
+	// 	const message = document.querySelector('.js-login-message');
+	// 	message.innerHTML = date;
+	// };
+
+	
 
 
 })();
