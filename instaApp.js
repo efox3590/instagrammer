154181d9b03e4021ab0802a2b00ff6150ca6c5dd
“efox3590” <emily.r.fox@hotmail.com>
2017-04-20 19:05:51 -0400

    working db

--- instaApp.js
+++ instaApp.js
@@ -1,24 +1,26 @@
-const db = require('sqlite');
-const DB_NAME = './database.sqlite';
+// const db = require('sqlite');
+// const DB_NAME = './database.sqlite';
 
 const instaApp = {};
 
+instaApp.init = function (db) {
+    instaApp.db = db;
+}
 // Get all users + their activity
 instaApp.getUsers = () => {
-    return db.all(`SELECT * FROM user
-                    INNER JOIN activity ON activity.user_id = user.id`)
+    return instaApp.db.all(`SELECT * FROM user`)
 };
 
 // Get a specified user via user.id + their activity
 instaApp.getUser = (user_id) => {
-    return db.all(`SELECT * FROM user 
-            INNER JOIN activity ON activity.user_id = user.id
+    return instaApp.db.all(`SELECT * FROM user 
+            INNER JOIN activity ON activity.id = user.id
             WHERE user.id = ${user_id}`)
 };
 
 // Get users that $user_id follows
 instaApp.getFollowed = (user_id) => {
-    return db.all(`SELECT 
+    return instaApp.db.all(`SELECT 
                 user.first_name AS user_fname,
                 user.last_name AS user_lname,
                 activity.image_url AS image,
@@ -31,27 +33,27 @@ instaApp.getFollowed = (user_id) => {
 
 // Create a post
 instaApp.createPost = (user_id, req) => {
-    return db.run(`INSERT INTO activity (user_id, image_url, descr) values (${user_id}, $image_url, $descr)`, req)
+    return instaApp.db.run(`INSERT INTO activity (user_id, image_url, descr) values (${user_id}, $image_url, $descr)`, req)
 };
 
 // Follow a user
 instaApp.followUser = (user_id, followed_id) => {
-    return db.run(`INSERT INTO followers (user_id, follow_id) VALUES (${user_id}, ${follow_id})`)
+    return instaApp.db.run(`INSERT INTO followers (user_id, follow_id) VALUES (${user_id}, ${follow_id})`)
 };
 
 // Edit a post
 instaApp.updatePost = (post_id, updatedText) => {
-    return db.run(`UPDATE activity SET descr = ${updatedText} WHERE ID = ${post_id}`)
+    return instaApp.db.run(`UPDATE activity SET descr = ${updatedText} WHERE ID = ${post_id}`)
 };
 
 // Delete a post
 instaApp.deletePost = (post_id) => {
-    return db.run(`DELETE FROM activity WHERE ID = ${post_id}`)
+    return instaApp.db.run(`DELETE FROM activity WHERE ID = ${post_id}`)
 };
 
 // Unfollow a user
 instaApp.unfollow = (user_id, followed_id) => {
-    return db.run(`DELETE FROM follower WHERE user_id = ${user_id} AND follow_id = ${follow_id}`)
+    return instaApp.db.run(`DELETE FROM follower WHERE user_id = ${user_id} AND follow_id = ${follow_id}`)
 };
 
 module.exports = instaApp
