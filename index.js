aaa5c1cc133b755c79daccb7737692b2078c837a
Emily Fox <emily.r.fox@hotmail.com>
2017-04-20 21:43:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ const app = express();
 
 const apiRoutes = require('./apiRoutes');
 
-const passport = require('./passport')
 
 const db = require('sqlite');
 const DB_NAME = './database.sqlite';
 
+const parser = require('body-parser');
+app.use(parser.json())
+
+const expressSession = require('express-session');
+app.use(expressSession({
+	secret: 'FOBAR'
+}));
+
 app.use('/', express.static('public'));
+
+const passport = require('./passport')(app, db);
+
+
+// eveything here is stuff that needs to be authenticated to be used
 app.use('/api', apiRoutes(db))
 
 
